49e23f7acc32a63228a73e314cb8ad0a359421cf
MrRushCH <marc.rusch@hotmail.ch>
2021-05-06 10:17:33 +0200

    Add firebase config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import config from "./firebase/firebaseConfig";
+import firebase from "firebase";
+
+firebase.initializeApp(config);
+const database = firebase.database();
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
     </div>
   );
 }
