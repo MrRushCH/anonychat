8eec2a6ae8fffa67c9df1e4a414efc98e6eba979
Marc Rusch <marc.rusch@hotmail.ch>
2021-05-12 10:25:51 +0200

    Modify Room and App

--- src/Room.js
+++ src/Room.js
@@ -1,31 +1,29 @@
-import firebase from "firebase";
-import config from "./firebase/firebaseConfig";
-
-firebase.initializeApp(config);
-const database = firebase.database();
+import database from "./firebase/firebaseConfig";
 
 class Room {
     constructor() {
         this.rid = this.getRoomId();
     }
     getRoomId() {
-        database.ref("rooms/").limitToLast(1).once("value").then((val) => {
-            if(val.val()[0]["memberCount"] > 10) {
-                this.createNewRoom();
-            } else {
-                this.rid = val.val()[0]["rid"];
-                this.messages = val.val()[0]["messages"];
-                database.ref(`rooms/${this.rid}/memberCount`).set(database.ref(`rooms/${this.rid}/memberCount`).get()+1)
-            }
+        database.ref("rooms/").get().then((res) => {
+            res.val().forEach((item) => {
+                if(item["memberCount"] < 10) {
+                    return item["rid"];
+                }
+            })
+            this.createNewRoom();
         })
     }
     createNewRoom() {
-        database.ref("rooms/").limitToLast(1).once("value").then((val) => {
-            const rid = val.val()[0]["rid"]+1;
+        database.ref("rooms/").get().then((res) => {
+            const rid = res.val()[res.val().length-1]["rid"]+1;
             database.ref(`rooms/${rid}`).set({
                 rid: rid,
                 messages: []
             })
+            return rid
         })
     }
 }
+
+export default Room;
